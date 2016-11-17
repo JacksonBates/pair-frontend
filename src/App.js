6ddb63a83dc0a1edc34f61c2465f66b489cc45df
jacksonbates <malgalin@gmail.com>
2016-11-17 21:53:52 +1100

    create-react-app set up

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>FCC Remote Pairing Noticeboard</h2>
+        </div>
+        <p className="App-intro">
+          Coming soon.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
