6487be4bb512d78aaadc1dece88caa892f29f0f4
jacksonbates <malgalin@gmail.com>
2016-11-23 22:40:23 +1100

    adds post modal
    needs to implement success redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
 // import logo from './logo.svg';
 import './App.css';
 import Campers from './Campers';
@@ -7,8 +9,20 @@ class App extends Component {
 
   constructor() {
     super();
-    this.state = { campers: [] };
-  }
+    this.state = { 
+      campers: [],
+      showModal: false 
+    };
+
+    this.close = () => {
+      this.setState({ showModal: false });
+    };
+
+    this.open = () => {
+      this.setState({ showModal: true });
+    };
+}
+  
 
   componentWillMount() {
     fetch('https://enigmatic-dawn-95873.herokuapp.com/api/v1/posts')
@@ -20,10 +34,35 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>FCC Remote Pairing Noticeboard</h2>
-          <h3>{this.props.title}</h3>
+          <p><img className='fcc-logo' src="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo_transparent.png" alt="The Free Code Camp remote pairing board" />
+          <span className='header-text'>Remote Pairing Noticeboard</span></p>
+          <Button
+          bsStyle="primary"
+          bsSize="small"
+          onClick={this.open}
+        >
+          Add to board
+        </Button>
         </div>
         <div>
+          <Modal show={this.state.showModal} onHide={this.close}>
+            <Modal.Header>
+              <Modal.Title>Hello</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <form action='https://enigmatic-dawn-95873.herokuapp.com/api/v1/add' method='post'>
+                <label htmlFor='username'>Forum username</label>
+                <input name='username' id='username' type='text' /><br />
+                <label htmlFor='availableTime'>Available Time (HH:MM)</label>
+                <input name='availableTime' id='availableTime' type='text' /><br />
+                <label htmlFor='setup'>Preferred setup</label>
+                <input name='setup' id='setup' type='text' /><br />
+                <label htmlFor='interests'>Interests</label>
+                <input name='interests' id='interests' type='text' /><br />
+                <input className='btn btn-success' type='submit' value='Submit' />
+              </form>
+            </Modal.Body>
+          </Modal>
           {this.state.campers.map((camper, i) => <Campers camper={camper} key={i} />)}
         </div>
       </div>
--- src/Campers.js
+++ src/Campers.js
@@ -4,7 +4,7 @@ class Campers extends Component {
   render() {
     return (
       <div>
-        <img src={'https://forum.freecodecamp.com/user_avatar/forum.freecodecamp.com/' + this.props.camper.username + '/120/202_1.png'} alt={this.props.camper.username + '\'s profile image'} />
+        <img className='profile-images' src={'https://forum.freecodecamp.com/user_avatar/forum.freecodecamp.com/' + this.props.camper.username + '/120/202_1.png'} alt={this.props.camper.username + '\'s profile image'} />
         <p><b><a href={'https://forum.freecodecamp.com/users/' + this.props.camper.username}>{this.props.camper.username}</a></b></p>
         <p><i>{this.props.camper.interests}</i></p>
         <p>{this.props.camper.setup}</p>
