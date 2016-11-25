f03c76b8b423afc1efd9e801f1c58300a45d6554
jacksonbates <malgalin@gmail.com>
2016-11-25 12:56:24 +1100

    adds available time in local timezone

--- src/Campers.js
+++ src/Campers.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Button, Glyphicon } from 'react-bootstrap';
 
 class Campers extends Component {
+  constructor(props){
+    super(props);
+    this.state = { end: new Date(this.props.camper.endTime).toTimeString()};
+  }
+  
   render() {
     return (
       <div className='user-card'>
@@ -12,6 +17,8 @@ class Campers extends Component {
         <span className='user-card-interests'>{this.props.camper.interests}</span>
         <br />
         <span className='user-card-setup'>{this.props.camper.setup}</span>
+        <br />
+        <span className='user-card-availability'>Available until: {this.state.end}</span>
         <form action={'https://enigmatic-dawn-95873.herokuapp.com/api/v1/' + this.props.camper._id} id={this.props.camper._id} method='post'>
           <input type='hidden' name='idToDelete' id='idToDelete' value={this.props.camper_id} />
           <Button type='submit' className='circle-button' bsStyle="primary" bsSize="small">
