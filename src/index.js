62a0257b55bd677b7b6e1aa58cb45d549119f532
alichristensen <ali.christensen1@gmail.com>
2016-11-28 08:41:43 -0600

    removed commented out module import from index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-// import data from './mock-data.js';
 
 ReactDOM.render(
   <App title='Fetching from API works! Now working on posting data :)' />,
