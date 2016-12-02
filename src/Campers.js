d7176da5029649941eefa157b94b6fd78563f804
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-02 14:21:48 +0800

    Change <span> to <p>

--- src/Campers.js
+++ src/Campers.js
@@ -18,13 +18,10 @@ class Campers extends Component {
         <img className='profile-images' height="120" width="120" src={'https://forum.freecodecamp.com/user_avatar/forum.freecodecamp.com/' + this.props.camper.username + '/120/202_1.png'} alt={this.props.camper.username + '\'s profile image'} />
         </a>
         <div className='user-card-info'>
-          <span className='user-card-name'><a href={'https://forum.freecodecamp.com/users/' + this.props.camper.username}>{this.props.camper.username}</a></span>
-          <br />
-          <span className='user-card-interests'>{this.props.camper.interests}</span>
-          <br />
-          <span className='user-card-setup'>Preferred Pairing Technology(s): {this.state.techSetup}</span>
-          <br />
-          <span className='user-card-availability'>Available until: {this.state.end}</span>
+          <p className='user-card-name'><a href={'https://forum.freecodecamp.com/users/' + this.props.camper.username}>{this.props.camper.username}</a></p>
+          <p className='user-card-interests'>{this.props.camper.interests}</p>
+          <p className='user-card-setup'>Preferred Pairing Technology(s): {this.state.techSetup}</p>
+          <p className='user-card-availability'>Available until: {this.state.end}</p>
           <form action={'https://enigmatic-dawn-95873.herokuapp.com/api/v1/' + this.props.camper._id} id={this.props.camper._id} method='post'>
             <input type='hidden' name='idToDelete' id='idToDelete' value={this.props.camper_id} />
             <Button type='submit' className='circle-button' bsStyle="danger" bsSize="small">
