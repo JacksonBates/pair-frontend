f5d42a87edb17b7245daf9c00bf1f6feab86d98d
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-02 13:58:32 +0800

    Wrap user card info in a div

--- src/Campers.js
+++ src/Campers.js
@@ -17,20 +17,21 @@ class Campers extends Component {
         <a href={'https://forum.freecodecamp.com/users/' + this.props.camper.username}>
         <img className='profile-images' height="120" width="120" src={'https://forum.freecodecamp.com/user_avatar/forum.freecodecamp.com/' + this.props.camper.username + '/120/202_1.png'} alt={this.props.camper.username + '\'s profile image'} />
         </a>
-        <br />
-        <span className='user-card-name'><a href={'https://forum.freecodecamp.com/users/' + this.props.camper.username}>{this.props.camper.username}</a></span>
-        <br />
-        <span className='user-card-interests'>{this.props.camper.interests}</span>
-        <br />
-        <span className='user-card-setup'>Preferred Pairing Technology(s): {this.state.techSetup}</span>
-        <br />
-        <span className='user-card-availability'>Available until: {this.state.end}</span>
-        <form action={'https://enigmatic-dawn-95873.herokuapp.com/api/v1/' + this.props.camper._id} id={this.props.camper._id} method='post'>
-          <input type='hidden' name='idToDelete' id='idToDelete' value={this.props.camper_id} />
-          <Button type='submit' className='circle-button' bsStyle="danger" bsSize="small">
-            Delete Post <Glyphicon glyph='trash' />
-          </Button>
-        </form>
+        <div className='user-card-info'>
+          <span className='user-card-name'><a href={'https://forum.freecodecamp.com/users/' + this.props.camper.username}>{this.props.camper.username}</a></span>
+          <br />
+          <span className='user-card-interests'>{this.props.camper.interests}</span>
+          <br />
+          <span className='user-card-setup'>Preferred Pairing Technology(s): {this.state.techSetup}</span>
+          <br />
+          <span className='user-card-availability'>Available until: {this.state.end}</span>
+          <form action={'https://enigmatic-dawn-95873.herokuapp.com/api/v1/' + this.props.camper._id} id={this.props.camper._id} method='post'>
+            <input type='hidden' name='idToDelete' id='idToDelete' value={this.props.camper_id} />
+            <Button type='submit' className='circle-button' bsStyle="danger" bsSize="small">
+              Delete Post <Glyphicon glyph='trash' />
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
