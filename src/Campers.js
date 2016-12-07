2ce5eae01144fffb1d2079f1887e9771b4fedd53
jacksonbates <malgalin@gmail.com>
2016-12-07 23:48:57 +1100

    removes user card on delete without refreshing / redirecting

--- src/Campers.js
+++ src/Campers.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Glyphicon } from 'react-bootstrap';
+import axios from 'axios';
 
 class Campers extends Component {
   constructor(props){
@@ -8,11 +9,28 @@ class Campers extends Component {
     var setup = this.props.camper.setup;
 
     this.state = { end: (date.substr(0,5) + ' ' + date.substr(9, date.length)), 
-                   techSetup: Array.isArray(setup) ? setup.join(', ') : setup};
+                   techSetup: Array.isArray(setup) ? setup.join(', ') : setup,
+                   id: this.props.camper_id,
+                   display: true };
+  
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   
+  handleSubmit(e) {
+    e.preventDefault();
+    const url = 'https://enigmatic-dawn-95873.herokuapp.com/api/v1/posts/' + this.props.camper._id;
+    
+    axios.delete(url).then( res => {
+      if ( res.status === 204 ) {
+        // remove this from DOM without a refresh...
+        this.setState( { display: false });
+      }
+    }).catch(e => console.log(e))
+  }
+
   render() {
-    return (
+    if (this.state.display === false) return null;
+    else return (
       <div className='user-card'>
         <a href={'https://forum.freecodecamp.com/users/' + this.props.camper.username}>
         <img className='profile-images' height="120" width="120" src={'https://forum.freecodecamp.com/user_avatar/forum.freecodecamp.com/' + this.props.camper.username + '/120/202_1.png'} alt={this.props.camper.username + '\'s profile image'} />
@@ -22,7 +40,7 @@ class Campers extends Component {
           <p className='user-card-interests'>{this.props.camper.interests}</p>
           <p className='user-card-setup'><b>Preferred Pairing Technology(s):</b> {this.state.techSetup}</p>
           <p className='user-card-availability'><b>Available until:</b> {this.state.end}</p>
-          <form action={'https://enigmatic-dawn-95873.herokuapp.com/api/v1/' + this.props.camper._id} id={this.props.camper._id} method='post'>
+          <form onSubmit={this.handleSubmit}>
             <input type='hidden' name='idToDelete' id='idToDelete' value={this.props.camper_id} />
             <Button type='submit' className='circle-button' bsStyle="danger" bsSize="small">
               Delete Post <Glyphicon glyph='trash' />
