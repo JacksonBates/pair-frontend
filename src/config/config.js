d2751f78e309d3ed378b5a4d260d6b022d6049ac
jacksonbates <malgalin@gmail.com>
2016-12-08 22:29:02 +1100

    moves config to src and server to route

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ let server = 'https://enigmatic-dawn-95873.herokuapp.com';
 const env = process.env.NODE_ENV;
 
 if (env === 'development') {
-  // server = 'http://localhost:3001';
+  server = 'http://localhost:3001';
 }
 
 module.exports = server;
