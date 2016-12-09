5329fa58f70018c1b88b585344bcc93e6412e923
fotonist <hasan.sonmez75@gmail.com>
2016-12-09 20:43:26 +0300

    app connection link uncommented

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   // res.setHeader('Access-Control-Allow-Credentials', true);
   next();
