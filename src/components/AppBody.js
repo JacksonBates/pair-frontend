54ea6b197b18c2dca55baacda3d7f009745bbd93
Gregory Smith <crashingwaves@fea.st>
2016-12-08 17:13:27 -0700

    Componetize AppBody

--- src/components/AppBody.js
+++ src/components/AppBody.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import Campers from './Campers';
+import CamperList from './CamperList';
 import InfoModal from './InfoModal';
 import AddModal from './AddModal';
 
 const AppBody = props => {
   return (
     <div className='App-body'>
-      <div className='container'>
-        <div className='row'>
-          {props.campers.map((camper, i) => <div key={i} className='col-xs-12 col-md-6'><Campers camper={camper} key={i} /></div>)}
-        </div>
-      </div>
-      <AddModal showModal={props.showModal} onHide={props.close} handleSubmit={props.handleSubmit} handleChange={props.handleChange} username={props.username} availableTime={props.availableTime} interests={props.interests} />
+      <CamperList campers={props.campers}/>
+      <AddModal
+    showModal={props.showModal}
+    onHide={props.close}
+    handleSubmit={props.handleSubmit}
+    handleChange={props.handleChange}
+    username={props.username}
+    availableTime={props.availableTime}
+    interests={props.interests} />
       <InfoModal showInfo={props.showInfo} close={props.close}/>
     </div>
     );
