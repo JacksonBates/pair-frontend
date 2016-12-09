fec863481398f1d6d323da572d2ccbc1d08b9e5c
Gregory Smith <crashingwaves@fea.st>
2016-12-08 18:08:33 -0700

    Add key

--- src/components/ModalSelectionList.js
+++ src/components/ModalSelectionList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ModalSelection from './ModalSelection';
 
 const ModalSelectionList = props => {
-  const modalSelections = props.modalSelections.map(selection => <ModalSelection title={selection} handleChange={props.handleChange} />)
+  const modalSelections = props.modalSelections.map(selection => <ModalSelection title={selection} handleChange={props.handleChange} key={selection}/>)
   return (
     <fieldset>
       <legend htmlFor="setup[]">Preferred Pairing Technology:</legend>
