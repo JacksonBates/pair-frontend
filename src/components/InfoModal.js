0f5db48d3ac622f95797118198beab50290ef3aa
Chris Gray <cmg_business@hotmail.com>
2016-12-30 01:04:45 +0000

    Updated info modal to be more helpful for new users

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -8,18 +8,25 @@ const InfoModal = props => {
       <AppModalHeader title="About" />
       <Modal.Body>
         <p>This pair noticeboard in intended only for use by FCC Forum participants - it doesn't work without a valid forum account.</p>
-        <p>This was made in response to the week long <a href='https://forum.freecodecamp.com/t/computer-frontend-web-development-challenge-november-16-to-26/55986'>front end challenge</a> organised by @P1xt.</p>
-        <h1>Technology used</h1>
-        <p>The front end is built with <b>React</b>, in firm defiance of best practices, and uses an unholy mix of <b>react-bootstrap</b>, and regular <b>Bootstrap</b>.</p>
-        <p>The back end is built with <b>NodeJS</b>, <b>Express</b> and <b>MongoDB</b>, with free, sandbox/hobby tier hosting from <b>Heroku</b> and <b>mLab</b>.</p>
-        <h1>Repos</h1>
-        <p>The front end repo is here: <a href='https://github.com/jacksonbates/pair-frontend'>Front end</a>.</p>
-        <p>The back end repo is here: <a href='https://github.com/jacksonbates/pair-backend'>Back end</a>.</p>
-        <p>Neither have been refactored or tidied, yet...</p>
-        <h1>Notes</h1>
-        <p>I suspect I am using React all wrong - especially with the way I am dealing with form submissions, forcing redirects an refreshes all over the place. Doesn't seem very Reacty to me...</p>
-        <h1>Further Development</h1>
-        <p>I am open to further development of this idea. Now that the project is finished in the eyes of the 'contest', I am open to working with any collaborators. PM me on the forum: <a href='https://forum.freecodecamp.com/users/jacksonbates'>JacksonBates</a></p>
+        <p>This is meant to enable FCC members to pair up on projects and advertise their availability to other FCC members.</p>
+        <h3>How To Use:</h3>
+        <ol>
+          <li>Click the add bottom at the bottom of the screen</li>
+          <li>On the dialogue screen that pops up you'll be presented with a range of options, these are outlined below:</li>
+            <ul>
+              <li>Forum Username: Use the same handle you use on the forum so that people can see who is advertising their time</li>
+              <li>Length of time available: How much time you actually have to pair in hours, so if you have an hour to spare enter it as 01:00</li>
+              <li>Preferred Pairing Technology: What would you like to use to pair and share screens</li>
+              <li>Other: This field doesn't currently work and is being looked at</li>
+              <li>Interests: This can be what you are interesting in covering while pairing as well as any other interests</li>
+
+            </ul>
+            <li>As a minimum fill out the forum username, length of time available and preferred technology</li>
+            <li>Once you have filled out the required fields click submit and wait for someone to respond to your advert</li>
+        </ol>
+        <h3>Get in touch</h3>
+        <p>Any questions or if you want to collaborate on this then get in touch via <a href="https://forum.freecodecamp.com/users/jacksonbates">JacksonBates</a> on the forum</p>
+        <p></p>
       </Modal.Body>
     </Modal>
     );
