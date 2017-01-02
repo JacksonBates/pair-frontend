274fbe35bd54bca125977578ea89e0bb0e9183fb
Benjamin <benjaminvanzwienen@live.nl>
2017-01-02 15:40:49 +0100

    Fix not showing Pairing Technologies

--- src/components/ModalSelection.js
+++ src/components/ModalSelection.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ModalSelection = props => {
   return (
-    <p>{props.title} <input name="setup[]" type="checkbox" value="{props.title}" onChange={props.handleChange}/></p>
+    <p>{props.title} <input name="setup[]" type="checkbox" value={props.title} onChange={props.handleChange}/></p>
     );
 };
 
